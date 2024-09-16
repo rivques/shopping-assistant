aee51b251d65e1f3dd8f184ecdc9f67ae0efd2eb
rivques <38469076+rivques@users.noreply.github.com>
2024-09-16 14:08:21 -0400

    add mp3test

---
+++ mp3test.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+CircuitPython single MP3 playback example for Raspberry Pi Pico.
+Plays a single MP3 once.
+"""
+import board
+import audiomp3
+import audiopwmio
+
+audio = audiopwmio.PWMAudioOut(board.GP0)
+
+decoder = audiomp3.MP3Decoder(open("lib/slow.mp3", "rb"))
+
+audio.play(decoder)
+while audio.playing:
+    pass
+
+print("Done playing!")
