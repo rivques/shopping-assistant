0b8d8aacf9019b8701f5b8b32e72531f5b677acc
rivques <38469076+rivques@users.noreply.github.com>
2024-09-25 13:01:13 -0400

    test with i2s speaker driver

---
+++ i2swavtest.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+# Example for how to play a .wav file using the Raspberry Pi Pico and the MAX98357 breakout board.
+# Derived from: https://learn.adafruit.com/adafruit-max98357-i2s-class-d-mono-amp/circuitpython-wiring-test
+#
+# See README.md for notes.
+
+import audiocore
+import board
+import audiobusio
+
+# Wav file must be 16bit, <= 22 Khz. You can convert your file using the Audacity app
+# See also https://learn.adafruit.com/adafruit-wave-shield-audio-shield-for-arduino/convert-files
+wave_file = open("./lib/sample-6s.wav", "rb")
+wave = audiocore.WaveFile(wave_file)
+
+# For Raspberry Pi Pico I used GP2 = data(DIN), GP3 = bit clock(BCLK), GP4 = word select (LRC)
+audio = audiobusio.I2SOut(board.GP3, board.GP4, board.GP2)
+# Or you can use different pins, just make sure the first 2 pins are next to each other
+#audio = audiobusio.I2SOut(board.GP14, board.GP15, board.GP2)
+
+
+while True:
+    audio.play(wave)
+    while audio.playing:
+        pass
---
+++ i2swebmp3.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2024 Jeff Epler for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+# Stream MP3 audio to I2S decoder
+#
+# Tested with:
+#
+#  * Adafruit Metro ESP32-S3
+#  * Adafruit Metro ESP32-S2
+#  * Adafruit Feather ESP32 V2
+
+import time
+
+import adafruit_connection_manager
+import adafruit_requests
+import audiobusio
+import audiomp3
+import board
+import wifi
+
+mp3_buffer = bytearray(16384)
+mp3_decoder = audiomp3.MP3Decoder("/lib/silence.mp3", mp3_buffer)
+
+pool = adafruit_connection_manager.get_radio_socketpool(wifi.radio)
+ssl_context = adafruit_connection_manager.get_radio_ssl_context(wifi.radio)
+requests = adafruit_requests.Session(pool, ssl_context)
+
+STREAMING_URL = "https://ice2.somafm.com/dronezone-128-mp3"
+
+i2s = audiobusio.I2SOut(board.GP3, board.GP4, board.GP2)
+
+with requests.get(STREAMING_URL, headers={"connection": "close"}, stream=True) as response:
+    mp3_decoder.file = response.socket
+    i2s.play(mp3_decoder)
+    while i2s.playing:
+        time.sleep(0.1)
